2957ae56ef088522445b180c6c60fb5cc3eb9072
Avery <avery@themarcos.net>
2023-03-04 14:28:36 -0500

    Update MainMenu.cs

--- ToBeNamed/Assets/MainMenu.cs
+++ ToBeNamed/Assets/MainMenu.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //Playing the Starting Scene using the build/ Scene index in the build settings
     public void PlayGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    //Closing/ Quitting the Game
     public void QuitGame()
     {
         Debug.Log("QUIT!");
